ec21845b5df1483416466cf2e1575b12e0fc48d4
nathy-yhtan <nathyreismoreira@gmail.com>
2026-06-26 11:34:13 -0300

    teste

---
+++ src/app/guards/login.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const loginGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  const email=sessionStorage.getItem("email")
+
+  if(!email) {
+    //alert("Usuário não autenticado.") -comentei pois funcionava no dashboard mas não no home
+    router.navigate([""])
+    return false
+  }
+  
+  return true;
+};
---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,75 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { CarTableComponent } from "../../components/car-table/car-table.component";
+import { DashboardService } from '../../services/dashboard.service';
+import { Veiculo, VinInfos } from '../../models/car';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [CardComponent, CarTableComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent implements OnInit {
+  dashboardService = inject(DashboardService)
+
+  veiculos: Veiculo[] = [] //uma lista de veiculos é iniciado vazio
+
+  veiculoSelecionado: Veiculo = { //é iniciado como veiculo que não existe
+    id: -1,
+    connected: 0,
+    volumetotal: 0,
+    softwareUpdates: 0,
+    vehicle: "",
+    img: "",
+    vin: "",
+  }
+
+  vinInfos: VinInfos = { id: -1, lat: 0, long: 0, nivelCombustivel: 0, odometro: 0, status: "" }
+
+  ngOnInit() {
+    this.dashboardService.getVeiculos().subscribe({
+      error: (err) => console.error(err),
+      next: (resposta: any) => {
+        this.veiculos = resposta.vehicles;
+
+        if (this.veiculos.length > 0) {
+          this.veiculoSelecionado = this.veiculos[0];
+        }
+
+        //^fora da live, mas agora vou adicionar coisas dela
+
+        this.dashboardService.getVinInfos(this.veiculoSelecionado.vin).subscribe({
+
+          error: () => { },
+          next: (vinInfos: any) => {
+            this.vinInfos = vinInfos
+          }
+        })
+      }
+    })
+  }
+
+  onChangeSelect(event: Event) {
+  const id = Number((event.target as HTMLSelectElement).value)
+  const veiculo = this.veiculos.find((veiculo) => veiculo.id === id)
+
+  if (veiculo) {
+    this.veiculoSelecionado = veiculo
+  }
+
+  //ADICIONADO PELA LIVE
+  this.dashboardService.getVinInfos(this.veiculoSelecionado.vin).subscribe({
+    
+      error: () => {},
+      next: (vinInfos) => {
+            this.vinInfos = this.vinInfos
+          }
+  })
+}
+
+onChangeVin() {
+
+}
+
+}
