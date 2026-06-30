83bb639b55a266a7b5b3ccaf6967677813252494
nathy-yhtan <nathyreismoreira@gmail.com>
2026-06-30 11:32:51 -0300

    home todo pronto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { MenuComponent } from "../../components/menu/menu.component";
-import { BoasVindasComponent } from "../../components/boas-vindas/boas-vindas.component";
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuComponent, BoasVindasComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  router = inject(Router)
+
+  isMenuAberto = false;
+
+  constructor(private router: Router) {}
+
+  goToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
 
   logout() {
     sessionStorage.clear()
